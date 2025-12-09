970ee42e5ab891658c930fb4d7245b62d1e8e210
z125522-marko-akmadza <z125522@shibaura-it.ac.jp>
2025-12-09 21:15:49 +0900

    Add files via upload

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+
+    int matrix[2][2];
+
+    for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+        scanf("%d", &matrix[i][j]);
+    }
+}
+
+   printf("You entered\n");
+for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+        printf("%d ", matrix[i][j]);
+    }
+     printf("\n");
+}
+
+    float determinant = matrix[0][0]*matrix[1][1]-matrix[1][0]*matrix[0][1];
+
+    printf("Determinant is %f\n", determinant);
+
+
+}
